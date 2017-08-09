bed3060364607e125e80d0f70bb25bcbaaf14cc7
knapptime9 <knappj84@yahoo.com>
2017-08-08 20:25:32 -0400

    fetch working submission.  to do: fix formatting

--- main.js
+++ main.js
@@ -6,3 +6,19 @@ let formSubmit = event => {
   let form = document.getElementClassName("searchInput")[0];
   form.addEventListener("submit", formSubmit);
 }
+
+fetch("https://crossorigin.me/http://www.recipepuppy.com/api/")
+  .then(
+    function(searchBox) {
+      searchBox.json().then(function(data) {
+        console.log(data.results)
+        for (let i = 0; i < data.results.length; i++) {
+          console.log(data.results[i])
+          let resultsContainer = document.createElement("resultsContainer");
+          document.body.appendChild(resultsContainer);
+          resultsContainer.innerHTML = `<h2>${data.results[i].title}</h2>
+              <li><img id="image" src=${data.results[i].thumbnail}></li>
+              <li><a>${data.results[i].href}</a>`
+        }
+      })
+    })
